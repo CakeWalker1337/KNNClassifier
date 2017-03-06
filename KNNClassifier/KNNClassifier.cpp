d64b1a728048f888e5e08186cb3dd2dd80e0e18b
BarD_RedStar <den_kovalev3@bk.ru>
2017-03-06 21:02:25 +0700

    FirstCommit

---
+++ KNNClassifier/KNNClassifier.cpp
@@ -0,0 +1,13 @@
+// KNNClassifier.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+    return 0;
+}
+
