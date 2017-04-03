b24c9ff2d6ddbbb6eaf54e9202037281666acec6
Max <max_kovalev@bk.ru>
2017-04-03 17:36:35 +0700

    fst

---
+++ KNNClassifier/KNNClassifier.cpp
@@ -0,0 +1,14 @@
+// KNNClassifier.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+	system("pause");
+    return 0;
+}
+
